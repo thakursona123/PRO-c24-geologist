07ae7faa6e6bc862416d884727d66974cc64b4e9
thakursona123 <86724152+thakursona123@users.noreply.github.com>
2021-07-07 15:19:26 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,29 @@
+class Stone {
+    constructor(x, y,width,height) {
+      var options = {
+        restitution:0.0,
+        friction:0.9,
+        density:12,
+        
+      };
+     this.body=Bodies.rectangle(x,y,width,height,options)
+     this.width=width;
+     this.height=height;
+
+     World.add(world,this.body);
+    };
+    display(){
+       var pos=this.body.position;
+       var angle=this.body.angle;
+       push();
+       translate(pos.x,pos.y);
+       rotate(angle);
+       //ectMode(CENTRE);
+       strokeWeight(4);
+       stroke("black");
+       fill("black");
+       rect(0,0,this.width,this.height);
+       pop();
+    };
+  };
+  
